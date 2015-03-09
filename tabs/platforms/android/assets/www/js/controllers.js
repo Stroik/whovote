80577e99abb60b8075a00b6e9fd0e81e027835ef
Stroik <augusto.marinaro@graion.com>
2015-03-09 15:10:53 -0300

    Ahora va tomando forma

---
+++ tabs/platforms/android/assets/www/js/controllers.js
@@ -0,0 +1,65 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $http, $localStorage, $stateParams, $ionicLoading, $ionicSlideBoxDelegate) {
+	$scope.$storage = $localStorage;
+	$ionicLoading.show({
+      template: 'Cargando...'
+    });
+	$http.get('http://www.noasa.com.ar/api/get_posts/?post_type=product').success(function(data){
+		if(!data){
+			$ionicLoading.show({
+		      template: 'Sin conexión',
+		      content: 'loading'
+		    });	
+		}
+		else{
+			$ionicLoading.hide();
+			$localStorage.posts = $scope.posts = data.posts;
+			$ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
+			console.log(data.posts);
+		}
+	}).error(function(err){
+		$ionicLoading.show({
+		      template: 'Sin conexión',
+		      content: 'loading'
+		});
+	});
+	$scope.myActiveSlide = 1;
+	
+})
+
+.controller('DashidCtrl', function($scope, $http, $localStorage, $stateParams, $ionicLoading){
+	$scope.$storage = $localStorage;
+	$ionicLoading.show({
+      template: 'Cargando...',
+      content: 'loading'
+    });
+
+	$http.get('http://www.noasa.com.ar/api/get_post/?post_type=product&slug='+ $stateParams.postSlug).success(function(data){
+		$ionicLoading.hide();
+		$localStorage.post = $scope.post = data.post;
+		console.log(data.post);
+	}).error(function(err){
+		$ionicLoading.show({
+	      template: 'Sin conexión',
+	      content: 'loading'
+	    });
+	});
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
